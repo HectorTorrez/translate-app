db77a8a1c6a06a76930ffcf7b84cf3e6aa36b19b
Hector <torrezhectorb@gmail.com>
2023-12-12 15:46:37 -0600

    chore: Add language support

---
+++ src/utils/languages.ts
@@ -0,0 +1,6 @@
+export const languages = [
+  { id: crypto.randomUUID(), name: "Detect Language", code: "auto" },
+  { id: crypto.randomUUID(), name: "English", code: "en" },
+  { id: crypto.randomUUID(), name: "France", code: "fr" },
+  { id: crypto.randomUUID(), name: "Spanish", code: "es" },
+];
